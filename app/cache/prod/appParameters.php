f4de4f0ab3f2c1e9529eefe68204b96d800cb2c7
Mickaël Blondel <mickael.blondel7@gmail.com>
2018-03-14 22:49:23 +0100

    First commit

---
+++ app/cache/prod/appParameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'sardine',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => '',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => '928xrOp1Z49rADM2FFDGKRMK90oF8cY1m34GdNZrJ0QdugjNCSByBsYs',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2018-03-09',
+    'locale' => 'fr-FR',
+    'cookie_key' => 'PIwB7iHznW9uPtWtTmjgrkwiEzsn1vewogKnG278bjCFsUtYqo0z1iwI',
+    'cookie_iv' => 'dDeXJKaK',
+    'new_cookie_key' => 'def000004a1800745a7d537061a12049c2ecf2640ab93aa28c1ff30e9e65f146b5fd383866325ac163345dd3d4670240030f03fb3b64bebf36a4d4313a7b52b09ad4c409',
+  ),
+);
